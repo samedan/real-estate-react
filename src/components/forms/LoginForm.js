6eb8d36f8a3a03d76f80757cc73690201b17393f
samedan <same.dan@gmail.com>
2020-05-02 12:13:41 +0200

    Finished Validation(custom) Form

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, ErrorMessage } from 'react-hook-form';
+import FormError from './FormError';
 
 const EMAIL_PATTERN = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const Error = ({ children }) => (
+  <div className="alert alert-danger">{children}</div>
+);
 
 const LoginForm = ({ onSubmit }) => {
   const { register, handleSubmit, errors } = useForm();
@@ -11,41 +16,46 @@ const LoginForm = ({ onSubmit }) => {
       <div className="form-group">
         <label htmlFor="email">Email</label>
         <input
-          ref={register({ required: true, pattern: EMAIL_PATTERN })}
+          ref={register({
+            required: 'Email is required',
+            pattern: {
+              value: EMAIL_PATTERN,
+              message: 'Invalid email format',
+            },
+          })}
           noValidate
           name="email"
           type="email"
           className="form-control"
           id="email"
         />
-        {errors.email && (
-          <div className="alert alert-danger">
-            {errors.email.type === 'required' && <span>Email is required</span>}
-            {errors.email.type === 'pattern' && (
-              <span>Not valid email format</span>
-            )}
-          </div>
-        )}
+
+        {/* <ErrorMessage as={<Error />} errors={errors} name="email">
+          {({ message }) => <p>{message}</p>}
+        </ErrorMessage> */}
+        <FormError errors={errors} name="email">
+          {(message) => <p>{message}</p>}
+        </FormError>
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
         <input
-          ref={register({ required: true, minLength: 6 })}
+          ref={register({
+            required: 'Password is required',
+            minLength: {
+              value: 6,
+              message: 'Minimum length of the password is 6 characters.',
+            },
+          })}
           name="password"
           type="password"
           className="form-control"
           id="password"
         />
-        {errors.password && (
-          <div className="alert alert-danger">
-            {errors.password.type === 'required' && (
-              <span>Password is required</span>
-            )}
-            {errors.password.type === 'minLength' && (
-              <span>Minimum length of the password is 6 characters.</span>
-            )}
-          </div>
-        )}
+
+        <ErrorMessage as={<Error />} errors={errors} name="password">
+          {({ message }) => <p>{message}</p>}
+        </ErrorMessage>
       </div>
       <button type="submit" className="btn btn-bwm-main">
         Submit
